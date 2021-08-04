054a53df25d380e8646cc589e5e8ff9d3e712e45
antoninocalisto <antuninosantos@gmail.com>
2021-08-04 18:39:58 -0300

    Iniciando desenvolvimento de ferramenta de debug

--- typescript/src/server.ts
+++ typescript/src/server.ts
@@ -4,7 +4,13 @@ import express from 'express';
 
 const app = express() 
 
+app.post("/courses", (request, response)=>{
+    console.log(request.body);
+    const {name} = request.body;
+    return response.json({name});
+
+});
 
 app.get("/", createCourse); 
 
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running!")); 
